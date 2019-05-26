91186eae767dc9c35f5edd66f2a9936aeddcbd13
ronaldo <ronaldo@cheesetea.com>
2019-05-27 00:04:01 +0200

    Z80EMU: Print para la memoria

--- c/z80emu/src/Memory.cpp
+++ c/z80emu/src/Memory.cpp
@@ -1,4 +1,43 @@
+#include <iomanip>
+#include <cstring>
 #include <Memory.hpp>
 
 namespace Z80CPP {
+
+Memory::Memory(uint16_t size) : m_size(size) {
+   m_bytes = std::make_unique<uint8_t[]>(size);
+}
+
+void 
+Memory::fill(uint8_t v) {
+   std::memset(m_bytes.get(), v, m_size);
+}
+
+uint8_t&
+Memory::operator[](uint16_t pos) {
+   if (pos > m_size) throw std::out_of_range("[]: Requested memory location is out of range\n");
+   
+   return m_bytes[pos];
+}
+
+void
+Memory::print(std::ostream& out, uint16_t pos, uint16_t blocks) const {
+   if (pos > m_size || pos + 16*blocks > m_size) throw std::out_of_range("PRINT: Requested memory location is out of range\n");
+   
+   ++blocks;
+   uint8_t* pmem = &m_bytes[pos];
+   while(--blocks) {
+      out << std::hex << std::setw(4) << std::setfill('0');
+      out << pos << "|";
+      uint8_t i = 17;
+      while(--i) { 
+         out << " " << std::setw(2) << std::setfill('0');
+         out << static_cast<uint16_t>(*pmem);
+         ++pmem;
+      }
+      pos += 16;
+      out << " |\n";
+   }
+}
+
 }; // Namespace Z80CPP
--- c/z80emu/src/Memory.hpp
+++ c/z80emu/src/Memory.hpp
@@ -2,27 +2,20 @@
 
 #include <memory>
 #include <cstdint>
-#include <cstring>
+#include <iostream>
 
 namespace Z80CPP {
 
 class Memory {
    std::unique_ptr<uint8_t[]> m_bytes;    // Raw Memory Bytes
-   uint32_t m_size = 0;                   // Size of the memory
+   uint16_t m_size = 0;                   // Size of the memory
 
 public:
-   Memory(uint32_t size) : m_size(size) {
-      m_bytes = std::make_unique<uint8_t[]>(size);
-   }
-
-   void fill(uint8_t v) {
-      std::memset(m_bytes.get(), v, m_size);
-   }
-
-   uint8_t& operator[](uint32_t pos) {
-      if (pos > m_size) throw std::out_of_range("Requested memory location is out of range\n");
-      return m_bytes[pos];
-   }
+   Memory(uint16_t size);
+   
+   void     fill(uint8_t v);
+   uint8_t& operator[](uint16_t pos);
+   void     print(std::ostream& out, uint16_t pos, uint16_t blocks) const;
 };
 
 }; // Namespace Z80CPP
