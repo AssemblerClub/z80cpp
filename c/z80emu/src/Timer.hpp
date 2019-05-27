13e14d88b4f8dfb8115e877318c94648c78986c5
ronaldo <ronaldo@cheesetea.com>
2019-05-27 15:35:46 +0200

    Z80EMU: Añadido timer simple usando std::chrono

---
+++ c/z80emu/src/Timer.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <chrono>
+
+template <typename UNIT>
+class Timer {
+   using Clock  = std::chrono::high_resolution_clock;
+   using TimePt = Clock::time_point;
+
+   TimePt m_start;
+
+public:
+   Timer() : m_start(Clock::now()) {}
+   void     reset()        { m_start = Clock::now(); }
+   double   passed() const {
+      using Duration = std::chrono::duration<double>;
+      Duration passed = std::chrono::duration_cast<Duration>(Clock::now() - m_start);
+      return passed.count();
+   }
+   UNIT     secs() const   { return passed();              }
+   UNIT     ms()   const   { return passed() * 1000;       }
+   UNIT     us()   const   { return passed() * 1000000;    }
+   UNIT     ns()   const   { return passed() * 1000000000; }
+};
