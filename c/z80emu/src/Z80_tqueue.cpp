87ac9d8fc2883621fb881536d21b38791796f45a
ronaldo <ronaldo@cheesetea.com>
2019-06-01 15:12:44 +0200

    Z80EMU: Lógica positiva y ciclos más ajustados   * Usando Virtual Z80 para ajustar el contenido de cada TState   * Señales individuales componibles en lógica positiva   * Cambiando la interfaz de Ciclos máquina para más facilidad   * Ampliado el tamaño de la cola de TStates para ejecutar   * Eliminado registro IR (Es la suma de I y R en realidad)   * Registros definidos en tqueue

--- c/z80emu/src/Z80_tqueue.cpp
+++ c/z80emu/src/Z80_tqueue.cpp
@@ -6,6 +6,7 @@ namespace Z80CPP {
 
 // Definition of static const signal groups
 const uint16_t MS_NOSIGNAL   = 0xFFFF;
+const uint16_t MS_M1         = (uint16_t)(~((uint16_t)Signal::M1));
 const uint16_t MS_MREQ       = (uint16_t)(~((uint16_t)Signal::MREQ));
 const uint16_t MS_MREQ_RD    = (uint16_t)(~((uint16_t)Signal::MREQ | (uint16_t)Signal::RD));
 const uint16_t MS_MREQ_WR    = (uint16_t)(~((uint16_t)Signal::MREQ | (uint16_t)Signal::WR));
@@ -13,44 +14,49 @@ const uint16_t MS_MREQ_RD_M1 = (uint16_t)(MS_MREQ_RD & ~((uint16_t)Signal::M1));
 const uint16_t MS_MREQ_RFSH  = (uint16_t)(~((uint16_t)Signal::MREQ | (uint16_t)Signal::RFSH));
 const uint16_t MS_RFSH       = (uint16_t)(~((uint16_t)Signal::RFSH));
 
+const uint16_t S_M1     = (uint16_t)Signal::M1;
+const uint16_t S_MREQ   = (uint16_t)Signal::MREQ;
+const uint16_t S_RD     = (uint16_t)Signal::RD;
+const uint16_t S_WR     = (uint16_t)Signal::WR;
+const uint16_t S_RFSH   = (uint16_t)Signal::RFSH;
 
 void 
-TVecOps::addM1(uint16_t& addr, uint8_t& in_reg) {
-   ops[last].set(&MS_MREQ_RD_M1, &addr     , nullptr, nullptr, TZ80Op(&Z80::fetch));
+TVecOps::addM1() {
+   ops[last].set(S_M1                 , &cpureg.PC, nullptr, nullptr, TZ80Op(&Z80::inc, cpureg.PC));
    inc(last);
-   ops[last].set(nullptr       , nullptr   , nullptr, nullptr, TZ80Op());
+   ops[last].set(S_M1 | S_MREQ | S_RD , nullptr   , nullptr, nullptr, TZ80Op());
    inc(last);
-   ops[last].set(&MS_MREQ_RFSH , nullptr   , nullptr, nullptr, TZ80Op(&Z80::data_in, in_reg));
+   ops[last].set(S_RFSH               , &cpureg.IR, nullptr, nullptr, TZ80Op(&Z80::decode));
    inc(last);
-   ops[last].set(&MS_RFSH      , nullptr   , nullptr, nullptr, TZ80Op(&Z80::decode));
+   ops[last].set(S_MREQ | S_RFSH      , nullptr   , nullptr, nullptr, TZ80Op(&Z80::inc7, cpureg.R));
    inc(last);
 }
 
 void 
-TVecOps::addM2Read(uint16_t& addr, uint8_t& in_reg, TZ80Op&& t0) {
+TVecOps::addM23Read(uint16_t& addr, uint8_t& in_reg, TZ80Op&& t0) {
    // Default Machine Re7ad Cycle (By default, reads from PC)
    //|      M2           |
    //| MREQ | WAIT| DIN  | 
    //|   RD |     |      | 
-   ops[last].set(&MS_MREQ_RD   , &addr     , nullptr, nullptr, std::move(t0));
+   ops[last].set(0            , &addr     , nullptr, nullptr, std::move(t0));
    inc(last);
-   ops[last].set(nullptr       , nullptr   , nullptr, nullptr, TZ80Op());
+   ops[last].set(S_MREQ | S_RD, nullptr   , nullptr, nullptr, TZ80Op());
    inc(last);
-   ops[last].set(&MS_NOSIGNAL  , nullptr   , nullptr, nullptr, TZ80Op(&Z80::data_in, in_reg));
+   ops[last].set(0            , nullptr   , nullptr, nullptr, TZ80Op(&Z80::data_in, in_reg));
    inc(last);
 }
 
 void 
-TVecOps::addM3Write(uint16_t& addr, uint8_t& data) {
+TVecOps::addM4Write(uint16_t& addr, uint8_t& data) {
    // Default Machine Write Cycle (By default, writes to (PC))
    //|      M3           |
    //| MREQ | WR  | DIN  | 
    //|     -DOUT-----    |
-   ops[last].set(&MS_MREQ     , &addr     , &data  , nullptr, TZ80Op());
+   ops[last].set(0            , &addr   , nullptr  , nullptr, TZ80Op());
    inc(last);
-   ops[last].set(&MS_MREQ_WR  , nullptr   , nullptr, nullptr, TZ80Op());
+   ops[last].set(S_MREQ       , nullptr , &data    , nullptr, TZ80Op());
    inc(last);
-   ops[last].set(&MS_NOSIGNAL , nullptr   , nullptr, nullptr, TZ80Op());
+   ops[last].set(S_MREQ | S_WR, nullptr , nullptr  , nullptr, TZ80Op());
    inc(last);
 }
 
--- c/z80emu/src/Z80_tqueue.hpp
+++ c/z80emu/src/Z80_tqueue.hpp
@@ -1,7 +1,47 @@
 #pragma once
 #include <array>
+#include <cstring>
 
 namespace Z80CPP {
+
+#if __BYTE_ORDER == __LITTLE_ENDIAN 
+   #define REGPAIR(NAME, HI, LO) \
+      union { uint16_t NAME; \
+              struct { uint8_t LO, HI; }; }
+#else
+   #define REGPAIR(NAME, HI, LO) \
+      union { uint16_t NAME; \
+              struct{ uint8_t HI, LO; }; }
+#endif
+
+//
+// Declares a struct with all Z80 Registers
+//
+struct Registers {
+   struct MainRegs {
+      REGPAIR(AF, A, F);
+      REGPAIR(BC, B, C);
+      REGPAIR(DE, D, E);
+      REGPAIR(HL, H, L);
+   } main;
+   struct AltRegs {
+      REGPAIR(AF, A, F);
+      REGPAIR(BC, B, C);
+      REGPAIR(DE, D, E);
+      REGPAIR(HL, H, L);
+   } alt;
+   REGPAIR(IX, IXh, IXl);
+   REGPAIR(IY, IYh, IYl);
+   REGPAIR(WZ,   W,   Z);
+   REGPAIR(SP,   S,   P);
+   REGPAIR(IR,   I,   R);
+   uint16_t PC;
+   Registers() { 
+      // Set all register memory to 0 at once
+      std::memset(this, 0, sizeof(Registers));
+   }
+};
+
 
 //
 // TZ80Op: Encapsulates an operation (member function) to be performed 
@@ -17,16 +57,20 @@ struct TZ80Op {
          NOP
       ,  VOID
       ,  _1PU8Ref
+      ,  _1PU16Ref
    };
    // Type aliases for function pointers
    using VOIDFp      = void(Z80::*)(void);
    using _1PU8RefFp  = void(Z80::*)(uint8_t&);
+   using _1PU16RefFp = void(Z80::*)(uint16_t&);
 
    // Explicit constructors for functions types
    explicit TZ80Op()                : m_type(Type::NOP) {} 
    explicit TZ80Op( VOIDFp f )      : m_type(Type::VOID), m_fv(f) {}
    explicit TZ80Op( _1PU8RefFp f, uint8_t& p ) 
       : m_type(Type::_1PU8Ref), m_f1u8r(f), m_f1u8r_p(&p) {}
+   explicit TZ80Op( _1PU16RefFp f, uint16_t& p ) 
+      : m_type(Type::_1PU16Ref), m_f1u16r(f), m_f1u16r_p(&p) {}
 
    // Set new function and type
    void reset() {
@@ -36,18 +80,24 @@ struct TZ80Op {
       m_type = Type::VOID;
       m_fv = f; 
    }
-   void set( _1PU8RefFp f, uint8_t& p0 ) { 
+   void set( _1PU8RefFp f, uint8_t& p ) { 
       m_type = Type::_1PU8Ref;
       m_f1u8r = f;
-      m_f1u8r_p = &p0;
+      m_f1u8r_p = &p;
+   }
+   void set( _1PU16RefFp f, uint16_t& p ) { 
+      m_type = Type::_1PU16Ref;
+      m_f1u16r = f;
+      m_f1u16r_p = &p;
    }
 
    // Call contained function
    void operator()(Z80& cpu) const {
       // Produce member function call depending on m_type
       switch(m_type) {
-         case Type::VOID:     (cpu.*m_fv)(); break;
-         case Type::_1PU8Ref: (cpu.*m_f1u8r)(*m_f1u8r_p); break;
+         case Type::VOID:      (cpu.*m_fv    )(); break;
+         case Type::_1PU8Ref:  (cpu.*m_f1u8r )(*m_f1u8r_p ); break;
+         case Type::_1PU16Ref: (cpu.*m_f1u16r)(*m_f1u16r_p); break;
          case Type::NOP: break;         
       }
    }
@@ -64,24 +114,48 @@ private:
          _1PU8RefFp  m_f1u8r;
          uint8_t*    m_f1u8r_p;
       };
+      // Type 2: Void function, 1 16-bits parameter by reference
+      struct {
+         _1PU16RefFp  m_f1u16r;
+         uint16_t*    m_f1u16r_p;
+      };
    };
+};
+
+//
+// Signal: Defines an individual Z80 signal 
+//  (the state of one of the pins)
+//
+enum class Signal : uint16_t {
+      M1    = 0x4000
+   ,  MREQ  = 0x2000
+   ,  IORQ  = 0x1000
+   ,  RD    = 0x0800
+   ,  WR    = 0x0400
+   ,  RFSH  = 0x0200
+   ,  HALT  = 0x0100
+   ,  WAIT  = 0x0080
 };
 
 //
 // TState: Status of the Z80 after a given TState
 //   It defines how pinouts should be, and an operation
 //   to be performed
+// WARNING: 
+//    Z80 uses negative logic for all in/out signals (0 = ON), except
+// data and address buses. However, this codifies signals as positive
+// logic (1 = ON) for clarity.
 //
 struct TState {
-   const uint16_t* signals = nullptr;
+         uint16_t  signals = 0;
    const uint16_t* addr    = nullptr; 
    const uint8_t*  data    = nullptr;
    const uint8_t*  io      = nullptr;
           TZ80Op   op      = TZ80Op();
    TState() = default;
-   TState(const uint16_t* s, const uint16_t* a, const uint8_t* d, const uint8_t* i, TZ80Op&& o)
+   TState(uint16_t s, const uint16_t* a, const uint8_t* d, const uint8_t* i, TZ80Op&& o)
       : signals(s), addr(a), data(d), io(i), op(std::move(o)) {}
-   void set(const uint16_t* s, const uint16_t* a, const uint8_t* d, const uint8_t* i, TZ80Op&& o) {
+   void set(uint16_t s, const uint16_t* a, const uint8_t* d, const uint8_t* i, TZ80Op&& o) {
       signals = s; addr = a; data = d;
       io = i; op = std::move(o);
    }
@@ -92,16 +166,18 @@ struct TState {
 // vector of pending operations (t-states) to perform
 //
 class TVecOps {
-   static constexpr uint8_t length = 16;
+   static constexpr uint8_t length = 24;
    std::array<TState, length> ops;
    uint8_t next = 0;
    uint8_t last = 0;
+   Registers& cpureg;
    
    inline void inc(uint8_t& v) { if(++v == length) v = 0;         }
 public:
-   void addM1(uint16_t& addr, uint8_t& in_reg);
-   void addM2Read(uint16_t& addr, uint8_t& in_reg, TZ80Op&& t0);
-   void addM3Write(uint16_t& addr, uint8_t& data);
+   TVecOps(Registers& cr) : cpureg(cr) {}
+   void addM1();
+   void addM23Read(uint16_t& addr, uint8_t& in_reg, TZ80Op&& t0);
+   void addM4Write(uint16_t& addr, uint8_t& data);
 
    void add(TState& newop) { ops[last] = newop; inc(last);        }
    const TState&  get()    { return ops[next];                    }
