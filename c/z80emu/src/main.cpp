a3b1c3a57c1d8cdb79a7b5402a1a7d24f4abb857
ronaldo <ronaldo@cheesetea.com>
2019-06-01 15:11:53 +0200

    Z80EMU: Señales consideradas con lógica pósitiva   * Eliminado código de constructor innecesario

--- c/z80emu/src/main.cpp
+++ c/z80emu/src/main.cpp
@@ -11,13 +11,7 @@ class Computer {
    Z80CPP::Memory   m_mem = Z80CPP::Memory(MS_MAXMEM);
 
 public:
-   Computer() {
-      //uint8_t program[16] = { 0x3E, 0x11, 0x06, 0x22, 0x0E, 0x33, 0x16, 0x44
-      //                     ,  0x1E, 0x55, 0x26, 0x00, 0x2E, 0x02, 0x36, 0x88 };
-      //uint8_t program[16] = {    0x3E, 0x11, 0x7F, 0x47, 0x4F, 0x57, 0x5F, 0x67
-      //                       ,   0x6F, 0x7D, 0x26, 0x00, 0x2E, 0x04, 0x36, 0x6C };
-      //std::memcpy(&m_mem[0], program, 16);
-   }
+   Computer() = default;
 
    void step() {
       // Tick the CPU
@@ -25,10 +19,10 @@ public:
 
       // Simulate a simple bus that connects to Memory through
       // RD and WR signals
-      if ( !m_cpu.signal(Z80CPP::Signal::MREQ)) {
-         if        ( !m_cpu.signal(Z80CPP::Signal::RD) ) {
+      if ( m_cpu.signal(Z80CPP::Signal::MREQ)) {
+         if        ( m_cpu.signal(Z80CPP::Signal::RD) ) {
             m_cpu.setData( m_mem[ m_cpu.address() ] );
-         } else if ( !m_cpu.signal(Z80CPP::Signal::WR) ) {
+         } else if ( m_cpu.signal(Z80CPP::Signal::WR) ) {
             m_mem[ m_cpu.address() ] = m_cpu.data();
          }
       }
