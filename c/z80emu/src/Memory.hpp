e0bb69eaf8fc468c61cf86d89e6925e0b8538383
ronaldo <ronaldo@cheesetea.com>
2019-06-02 17:51:45 +0200

    Z80EMU: Eliminado código de print de Memory     * Añadido operador[] constante para accesos const

--- c/z80emu/src/Memory.hpp
+++ c/z80emu/src/Memory.hpp
@@ -10,12 +10,14 @@ class Memory {
    std::unique_ptr<uint8_t[]> m_bytes;    // Raw Memory Bytes
    uint16_t m_size = 0;                   // Size of the memory
 
+   const uint8_t& get(uint16_t pos) const;
 public:
    Memory(uint16_t size);
    
    void     fill(uint8_t v);
-   uint8_t& operator[](uint16_t pos);
-   void     print(std::ostream& out, uint16_t pos, uint16_t blocks) const;
+   uint8_t&       operator[](uint16_t pos)         { return const_cast<uint8_t&>(get(pos)); }
+   const uint8_t& operator[](uint16_t pos) const   { return get(pos); }
+   uint16_t size() const { return m_size; }
 };
 
 }; // Namespace Z80CPP
